6f83e150ed7fc5f49d0edbd66725f1fe4f6208b9
jungho5045 <jungho5045@naver.com>
2020-02-02 23:47:11 +0900

    [refactor] "Chapter 06_JPA 수강 중, Spring Boot JPA, H2 Database 연동하기"

--- eatgo-api/src/main/java/kr/co/fastcampus/eatgo/domain/MenuItemRepository.java
+++ eatgo-api/src/main/java/kr/co/fastcampus/eatgo/domain/MenuItemRepository.java
@@ -1,8 +1,8 @@
 package kr.co.fastcampus.eatgo.domain;
 
-import org.springframework.data.repository.CrudRepository;
+        import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
+        import java.util.List;
 
 public interface MenuItemRepository extends CrudRepository<MenuItem, Long> {
     List<MenuItem> findAllByRestaurantId(Long retaurantid);
