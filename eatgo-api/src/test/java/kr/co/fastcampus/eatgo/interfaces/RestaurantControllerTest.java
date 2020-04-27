3d09ef8d2ed89c7fa2f1844deb1d0740c01be5ce
jungho5045 <jungho5045@naver.com>
2020-04-27 18:20:32 +0900

    [refactor] "Chapter 06_에러처리 수강 완료"
    1. URL or URI를 통해서 존재하지 않는 Resource에 접근 할 경우 Not Found Error가 발생, 이런경우 Exception(예외)을 이용하여 처리한다.
    2. HTTP Status의 경우 404(Not Found) Error를 이용하여 Exception을 처리한다.
    3. 실제로 예외를 던저주고 그 예외를 받아서 처리하는 @ControllerAdvice Annotation을 이용한다.

--- eatgo-api/src/test/java/kr/co/fastcampus/eatgo/interfaces/RestaurantControllerTest.java
+++ eatgo-api/src/test/java/kr/co/fastcampus/eatgo/interfaces/RestaurantControllerTest.java
@@ -3,14 +3,14 @@ package kr.co.fastcampus.eatgo.interfaces;
 import kr.co.fastcampus.eatgo.application.RestaurantService;
 import kr.co.fastcampus.eatgo.domain.MenuItem;
 import kr.co.fastcampus.eatgo.domain.Restaurant;
-import kr.co.fastcampus.eatgo.domain.RestaurantNotFondException;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import kr.co.fastcampus.eatgo.domain.RestaurantNotFoundException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.ArrayList;
@@ -24,41 +24,37 @@ import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@ExtendWith(SpringExtension.class)          // JUnit4 @RunWith 대신 JUnit5 @ExtendWith 사용, GET 요청시 어디에 요청할지 정한다(SpringExtention에 요청한다.)
-// @RunWith(SpringRunner.class)             // Spring을 이용하여 해당 Test를 실행 할 수 있게 @RunWith 어노테이션을 추가한다.
-@WebMvcTest(RestaurantController.class)     // RestaurantController.class를 Test한다고 명시할 수 있다.
-class RestaurantControllerTest {
+@RunWith(SpringRunner.class)
+@WebMvcTest(RestaurantController.class)
+public class RestaurantControllerTest {
 
-    @Autowired                                       // mvc객체를 만들지 않아도 알아서? SpringBoot에서 알아서 넣어줄 수 있도록 @Autowired 어노테이션을 선언한다.
-    private MockMvc mvc;                             // mvc객체를 이용할 수 있게 해준다.
+    @Autowired
+    private MockMvc mvc;
 
     @MockBean
     private RestaurantService restaurantService;
 
-    // 가게 목록
     @Test
-    public void list() throws Exception {            // perform에서 예외가 발생할 수 있어서 throws Exception을 해준다.
-        List<Restaurant> restaurants = new ArrayList<>();                                       // Mock Object(가짜 객체) 부분
+    public void list() throws Exception {
+        List<Restaurant> restaurants = new ArrayList<>();
         restaurants.add(Restaurant.builder()
                 .id(1004L)
                 .name("JOKER House")
                 .address("Seoul")
                 .build());
 
-
         given(restaurantService.getRestaurants()).willReturn(restaurants);
 
-        mvc.perform(get("/restaurants"))  // mvc객체를 이용하여 perform을 한다. perform은 restaurant에 대한 GET 요청이다.
-            .andExpect(status(). isOk())             // 또한 요청한 GET에 대해서 요청의 성공유무를 위해 andExpect(status(). isOk())를 한다.
-                .andExpect(content().string(         // 가게 이름이 나오도록 content()를 사용한다.
-                        containsString("\"id\":1004")       //
+        mvc.perform(get("/restaurants"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(
+                        containsString("\"id\":1004")
                 ))
                 .andExpect(content().string(
-                        containsString("\"name\":\"JOKER House\"")  //
-            ));
+                        containsString("\"name\":\"JOKER House\"")
+                ));
     }
 
-    // 가게 상세
     @Test
     public void detailWithExisted() throws Exception {
         Restaurant restaurant1 = Restaurant.builder()
@@ -66,9 +62,11 @@ class RestaurantControllerTest {
                 .name("JOKER House")
                 .address("Seoul")
                 .build();
-        MenuItem menuItem = MenuItem.builder()
+
+        MenuItem menuItem = (MenuItem.builder()
                 .name("Kimchi")
-                .build();
+                .build());
+
         restaurant1.setMenuItems(Arrays.asList(menuItem));
 
         Restaurant restaurant2 = Restaurant.builder()
@@ -83,17 +81,19 @@ class RestaurantControllerTest {
         mvc.perform(get("/restaurants/1004"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(
-                        containsString("\"id\":1004")       //
+                        containsString("\"id\":1004")
                 ))
                 .andExpect(content().string(
                         containsString("\"name\":\"JOKER House\"")
                 ))
-                .andExpect(content().string(containsString("Kimchi")));     // MenuItem 추가 부분
+                .andExpect(content().string(
+                        containsString("Kimchi")
+                ));
 
         mvc.perform(get("/restaurants/2020"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(
-                        containsString("\"id\":2020")       //
+                        containsString("\"id\":2020")
                 ))
                 .andExpect(content().string(
                         containsString("\"name\":\"Cyber Food\"")
@@ -101,20 +101,20 @@ class RestaurantControllerTest {
     }
 
     @Test
-    public void detailWithNotExisted() throws Exception {
+    public void detailWithNotExisted() throws Exception{
         given(restaurantService.getRestaurant(404L))
-                .willThrow(new RestaurantNotFondException(404L));
+                .willThrow(new RestaurantNotFoundException(404L));
+
         mvc.perform(get("/restaurants/404"))
                 .andExpect(status().isNotFound())
                 .andExpect(content().string("{}"));
     }
 
-    // 가게 추가
     @Test
     public void createWithValidData() throws Exception {
         given(restaurantService.addRestaurant(any())).will(invocation -> {
-           Restaurant restaurant = invocation.getArgument(0);
-           return Restaurant.builder()
+            Restaurant restaurant = invocation.getArgument(0);
+            return Restaurant.builder()
                     .id(1234L)
                     .name(restaurant.getName())
                     .address(restaurant.getAddress())
@@ -123,7 +123,7 @@ class RestaurantControllerTest {
 
         mvc.perform(post("/restaurants")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"name\":\"BeRyong\",\"address\":\"Busan\"}"))
+                .content("{\"name\":\"Beryong\", \"address\":\"Busan\"}"))
                 .andExpect(status().isCreated())
                 .andExpect(header().string("location", "/restaurants/1234"))
                 .andExpect(content().string("{}"));
@@ -135,36 +135,33 @@ class RestaurantControllerTest {
     public void createWithInvalidData() throws Exception {
         mvc.perform(post("/restaurants")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"name\":\"\",\"address\":\"\"}"))
+                .content("{\"name\":\"\", \"address\":\"\"}"))
                 .andExpect(status().isBadRequest());
     }
 
-    // 가게 수정
     @Test
     public void updateWithValidData() throws Exception {
         mvc.perform(patch("/restaurants/1004")
-            .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"name\":\"JOKER Bar\", \"address\":\"Busan\"}")        )
-                .andExpect(status().isOk());
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"name\":\"JOKER Bar\",\"address\":\"Busan\"}"))
+                .andExpect(status(). isOk());
 
         verify(restaurantService).updateRestaurant(1004L, "JOKER Bar", "Busan");
-
     }
 
     @Test
     public void updateWithInvalidData() throws Exception {
         mvc.perform(patch("/restaurants/1004")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"name\":\"\", \"address\":\"\"}")        )
-                .andExpect(status().isBadRequest());
+                .content("{\"name\":\"\",\"address\":\"\"}"))
+                .andExpect(status(). isBadRequest());
     }
 
     @Test
     public void updateWithoutName() throws Exception {
         mvc.perform(patch("/restaurants/1004")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"name\":\"\", \"address\":\"Busan\"}")        )
-                .andExpect(status().isBadRequest());
+                .content("{\"name\":\"\",\"address\":\"Busan\"}"))
+                .andExpect(status(). isBadRequest());
     }
-
 }
